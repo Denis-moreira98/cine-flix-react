804b92639c37f2beae2af25493cb6365ace87add
Denis Moreira <72985107+Denis-moreira98@users.noreply.github.com>
2023-11-24 17:14:22 -0300

    feat: alterando para logo svg

--- src/components/Header/index.js
+++ src/components/Header/index.js
@@ -1,13 +1,17 @@
 import "./header.css";
 import { Link } from "react-router-dom";
-import LogoImg from "../../assets/logo.png";
+// import LogoImg from "../../assets/logo.png";
+//import { logoSvg } from "../../assets/logo2.svg";
 
 function Header() {
    return (
       <header>
          <div className="nav-header">
             <Link className="logo" to="/">
-               <img src={LogoImg} alt="logo" />
+               <img
+                  src={require("../../assets/logo2.svg").default}
+                  alt="logo"
+               />
             </Link>
             <Link className="favoritos" to="/favoritos">
                Meus favoritos
