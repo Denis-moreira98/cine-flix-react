a7d0ebcd1cdf274b05d2228d49fa572c8556f519
Denis Moreira <72985107+Denis-moreira98@users.noreply.github.com>
2023-11-24 15:15:04 -0300

    feat: alteração do logo e bg do header

--- src/components/Header/index.js
+++ src/components/Header/index.js
@@ -1,15 +1,18 @@
 import "./header.css";
 import { Link } from "react-router-dom";
+import LogoImg from "../../assets/logo.png";
 
 function Header() {
    return (
       <header>
-         <Link className="logo" to="/">
-            Prime Flix
-         </Link>
-         <Link className="favoritos" to="/favoritos">
-            Meus Filmes
-         </Link>
+         <div className="nav-header">
+            <Link className="logo" to="/">
+               <img src={LogoImg} alt="logo" />
+            </Link>
+            <Link className="favoritos" to="/favoritos">
+               Meus favoritos
+            </Link>
+         </div>
       </header>
    );
 }
